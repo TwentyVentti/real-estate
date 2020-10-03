f35451635d8b35ac32877a99d71310bce630c71c
Andrew Carse <andrew.carse25@gmail.com>
2020-10-03 21:14:47 +1000

    Added loginActivity.java

--- app/src/main/java/com/example/myproject/User.java
+++ app/src/main/java/com/example/myproject/User.java
@@ -24,4 +24,12 @@ public class User implements Serializable {
         this.username = username;
         this.password = password;
     }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
