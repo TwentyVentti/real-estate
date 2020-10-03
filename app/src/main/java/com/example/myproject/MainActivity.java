1e32b54d28ec615f19d88bbdb8b8ddf85b07de6c
Andrew Carse <andrew.carse25@gmail.com>
2020-10-03 18:00:05 +1000

    Added onClickListeners to France and Italy

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView italyImageView = (ImageView) findViewById(R.id.italyImageView);
+        ImageView franceImageView = (ImageView) findViewById(R.id.franceImageView);
+        italyImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "You clicked on Italy!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+        franceImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "You clicked on France!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
+
 }
