34ce5cac52078e7754cae8444d2d4ce5e81499da
Andrew Carse <andrew.carse25@gmail.com>
2020-10-07 15:25:19 +1100

    Renamed mainpage to activity_cityselect.

--- app/src/main/java/com/example/myproject/SearchActivity.java
+++ app/src/main/java/com/example/myproject/SearchActivity.java
@@ -2,7 +2,6 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class SearchActivity extends AppCompatActivity {
@@ -11,11 +10,5 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        Intent languageIntent = getIntent();
-        String languageChosen = languageIntent.getStringExtra("Language");
     }
-
-
-
-
 }
