f1157202dd7262d6229fa46629ae5c255360aa30
Andrew Carse <andrew.carse25@gmail.com>
2020-10-10 15:38:47 +1100

    refactoring the home page

--- app/src/main/java/com/example/myproject/Phrase.java
+++ app/src/main/java/com/example/myproject/Phrase.java
@@ -7,22 +7,54 @@ public class Phrase implements Serializable {
     private String english;
     private String french;
     private String italian;
+    private String spanish;
+    private String dutch;
+
+    public Phrase(String english) {
+        this.english = english;
+        this.french = french;
+        this.italian = italian;
+        this.spanish = spanish;
+        this.dutch = dutch;
+    }
 
     public String getEnglish() {
         return english;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
     }
 
     public String getFrench() {
         return french;
     }
 
+    public void setFrench(String french) {
+        this.french = french;
+    }
+
     public String getItalian() {
         return italian;
     }
 
-    public Phrase(String english, String french, String italian) {
-        this.english = english;
-        this.french = french;
+    public void setItalian(String italian) {
         this.italian = italian;
+    }
+
+    public String getSpanish() {
+        return spanish;
+    }
+
+    public void setSpanish(String spanish) {
+        this.spanish = spanish;
+    }
+
+    public String getDutch() {
+        return dutch;
+    }
+
+    public void setDutch(String dutch) {
+        this.dutch = dutch;
     }
 }
