8da21a7a0e6946286b10a1355622ba4eb7a650cd
kanav <u6995005@anu.edu.au>
2020-10-10 16:33:18 +1100

    search button functionality changed

--- app/src/main/java/com/example/myproject/SearchActivity.java
+++ app/src/main/java/com/example/myproject/SearchActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class SearchActivity extends AppCompatActivity {
@@ -19,16 +20,21 @@ public class SearchActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.names));
 
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
+        Button button = (Button) findViewById(R.id.search);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewActivity();
+            }
+        });
 
 
     }
 
-    public void citySelectClicked(View v){
-        Intent intent = new Intent(SearchActivity.this,MainActivity.class);
+    public void openNewActivity() {
+        Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
-
 
 
 }
