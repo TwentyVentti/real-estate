ba6f9d1b62a2e59086a35fd18718a53fd407d62d
Andrew Carse <andrew.carse25@gmail.com>
2020-10-11 22:37:14 +1100

    Starting on parser. Progressing

---
+++ app/src/main/java/com/example/myproject/Models/IntExp.java
@@ -0,0 +1,17 @@
+package com.example.myproject.Models;
+
+import java.util.ArrayList;
+
+public class IntExp extends Exp {
+    private Integer stay;
+
+    public IntExp(Integer stay) { this.stay = stay; }
+
+    @Override
+    public String show() { return stay.toString(); }
+
+    @Override
+    public ArrayList<String> selection() {
+        return null;
+    }
+}
