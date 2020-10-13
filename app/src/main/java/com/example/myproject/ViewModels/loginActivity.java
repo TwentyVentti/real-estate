52eea5f7fd8249454122cd25854bae13f4695e35
Andrew Carse <andrew.carse25@gmail.com>
2020-10-13 14:47:51 +1100

    Cleaning up, hashmap implemented

--- app/src/main/java/com/example/myproject/ViewModels/loginActivity.java
+++ app/src/main/java/com/example/myproject/ViewModels/loginActivity.java
@@ -35,8 +35,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -54,17 +54,20 @@ public class loginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        emailText = (EditText)findViewById(R.id.emailText);
-        passwordText = (EditText)findViewById(R.id.passwordText);
-        progressBar = (ProgressBar)findViewById(R.id.progressBar);
-        signin = (Button)findViewById(R.id.login_button);
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+        progressBar = findViewById(R.id.progressBar);
+        signin = findViewById(R.id.login_button);
         signin.setOnClickListener(this);
         mAuth = FirebaseAuth.getInstance();
-        forgotpassword = (TextView)findViewById(R.id.textView);
+        forgotpassword = findViewById(R.id.textView);
         forgotpassword.setOnClickListener(this);
-        guest = (Button)findViewById(R.id.guest_user_button);
-
-
+        guest = findViewById(R.id.guest_user_button);
+        try {
+            phraseListHash = ObjectFromJSON();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void registerUser(View v) {
@@ -82,9 +85,7 @@ public class loginActivity extends AppCompatActivity implements View.OnClickList
             assert outerValues != null;
             JSONObject innerObj = ((JSONObject) outerValues.get(0));
             JSONArray innerNames = innerObj.names();
-            JSONArray innerValues = innerObj.toJSONArray(innerNames);
-//            JSONArray sections = (JSONObject) values.getJSONObject(0).toJSONArray();
-//            Object town = sections.getString("Around_Town");
+            assert innerNames != null;
             for (int i = 0; i < innerNames.length(); i++) {
                 JSONArray sectionArray = innerObj.getJSONArray((String) innerNames.get(i));
                 HashMap<Integer, Phrase> sectionHash = new HashMap<Integer, Phrase>();
@@ -106,9 +107,9 @@ public class loginActivity extends AppCompatActivity implements View.OnClickList
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        phraseListHash = phraseListHashMap;
         return phraseListHashMap;
     }
+
     public String loadJSONFromAsset() {
         String json = null;
         try {
@@ -117,64 +118,19 @@ public class loginActivity extends AppCompatActivity implements View.OnClickList
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-            json = new String(buffer, "UTF-8");
+            json = new String(buffer, StandardCharsets.UTF_8);
         } catch (IOException ex) {
             ex.printStackTrace();
             return null;
         }
         return json;
     }
-    @RequiresApi(api = Build.VERSION_CODES.O)
+
     public void guestSessionClicked(View v) throws IOException {
         Intent intent = new Intent(loginActivity.this, SearchActivity.class);
-        System.out.println("JSON ARRAY PHRASES:");
-        ArrayList<HashMap<Integer,Phrase>> phraseHash = ObjectFromJSON();
-        for (int i = 0; i <2; i++) {
-            HashMap<Integer,Phrase> hashMap = phraseHash.get(i);
-        }
-
         intent.putExtra("but",1);
         startActivityForResult(intent,1);
-
     }
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public List<Phrase> getFromJson() throws IOException {
-        Gson gson = new Gson();
-        JsonReader jsonReader= null;
-
-        final Type CUS_LIST_TYPE = new TypeToken<List<Phrase>>() {}.getType();
-        File f = new File(getCacheDir()+"/phrase_array.json");
-        FileOutputStream fos;
-        if (!f.exists()) try {
-
-            InputStream is = getAssets().open("phrase_array.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-
-
-            fos = new FileOutputStream(f);
-            fos.write(buffer);
-            fos.close();
-        } catch (Exception e) { throw new RuntimeException(e); }
-
-        FileReader fileReader = new FileReader(f.getName());
-
-        try{
-            jsonReader = new JsonReader(fileReader);
-        } catch (Exception ex){
-            ex.printStackTrace();
-        }
-        List<Phrase> x = gson.fromJson(String.valueOf(jsonReader),CUS_LIST_TYPE);
-        for (Phrase p: x) {
-            System.out.println(p.toString());
-        }
-
-        return gson.fromJson(String.valueOf(jsonReader),CUS_LIST_TYPE);
-    }
-
-
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -185,11 +141,9 @@ public class loginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.textView:
                 startActivity(new Intent(this,resetpasswordActivity.class));
                 break;
-
         }
 
     }
-
     private void userLogin() {
         String email = emailText.getText().toString().trim();
         String password = passwordText.getText().toString().trim();
