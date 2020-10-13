39d5dfe695cce3379c14122f679a132cda47a400
kanav <u6995005@anu.edu.au>
2020-10-14 02:28:47 +1100

    addings maps, api added

--- app/src/main/java/com/example/myproject/MapActivity.java
+++ app/src/main/java/com/example/myproject/MapActivity.java
@@ -1,14 +1,168 @@
 package com.example.myproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MapActivity extends AppCompatActivity {
+
+    Spinner spType;
+    Button btfind;
+    SupportMapFragment supportMapFragment;
+    GoogleMap map;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    double currentLat=0, currentLong=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        spType=findViewById(R.id.sp_type);
+        btfind=findViewById(R.id.bt_find);
+        supportMapFragment=(SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.google_map);
+
+        final String[] placeTypeList = {"mylocation","restaurants","bars","hotels"};
+
+        String[] placeNameList = {"Mylocation","Restaurants","Bars","Hotels"};
+
+        spType.setAdapter(new ArrayAdapter<>(MapActivity.this,
+                android.R.layout.simple_spinner_dropdown_item,placeNameList));
+
+        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            getCurrentLocation();
+        }
+        else {
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION},44);
+
+        }
+
+        btfind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int i = spType.getSelectedItemPosition();
+                String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" +
+                "?location=" + currentLat + "," + currentLong +
+                "&radius=5000" +
+                "&types=" + placeTypeList[i] +
+                "&sensor=true" +
+                "$key=" + getResources().getString(R.string.google_map_key);
+
+                new PlaceTask().execute(url);
+
+
+
+            }
+        });
+    }
+
+    private void getCurrentLocation() {
+        @SuppressLint("MissingPermission")
+        Task<Location> task= fusedLocationProviderClient.getLastLocation();
+
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+
+                if (location!=null)
+                {
+                    currentLat=location.getLatitude();
+                    currentLong=location.getLongitude();
+
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            map=googleMap;
+                            map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(currentLat,currentLong),10
+                            ));
+                        }
+                    });
+
+                }
+
+            }
+        });
+        {
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 44){
+            if(grantResults.length > 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
+                getCurrentLocation();
+        }
+    }
+
+    private class PlaceTask extends AsyncTask<String,Integer,String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                String data = downloadUrl(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    private String downloadUrl(String string) throws IOException {
+
+        URL url = new URL(string);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.connect();
+        InputStream stream = connection.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder builder = new StringBuilder();
+        String line = "";
+
+        while ((line = reader.readLine()) != null){
+
+            builder.append(line);
+        }
+
+        String data = builder.toString();
+        reader.close();
+        return data;
     }
 }
