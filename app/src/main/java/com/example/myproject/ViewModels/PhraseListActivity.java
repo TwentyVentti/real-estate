6d46e8ed8cebfa7fb9075c33104fec6d2269004c
Andrew Carse <andrew.carse25@gmail.com>
2020-10-15 01:29:34 +1100

    PhraseListActivity item tranformations are tricky

--- app/src/main/java/com/example/myproject/ViewModels/PhraseListActivity.java
+++ app/src/main/java/com/example/myproject/ViewModels/PhraseListActivity.java
@@ -5,13 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.myproject.Models.Phrase;
 import com.example.myproject.R;
+import com.example.myproject.ui.HomePage.HomePageFragment;
+
+import java.util.ArrayList;
 
 public class PhraseListActivity extends AppCompatActivity {
     TextView sectionTextView;
     String section;
+    ListView phraseListView;
+    ArrayAdapter<String> phraseArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +31,9 @@ public class PhraseListActivity extends AppCompatActivity {
                 finish();
             }
         };
+        phraseListView = findViewById(R.id.phraseListView);
         sectionTextView = findViewById(R.id.sectionTextView);
+//        phraseArrayAdapter = new ArrayAdapter<>(PhraseListActivity.this,android.R.layout.simple_list_item_1, sections);
         Intent homePageIntent = getIntent();
         try {
             section = homePageIntent.getStringExtra("Section").replaceAll("_", " ");
@@ -32,6 +42,20 @@ public class PhraseListActivity extends AppCompatActivity {
             ex.printStackTrace();
         }
         sectionTextView.setText(section);
+    }
+
+    private ArrayList<String> getPhrases(){
+        ArrayList<Phrase> phrases = new ArrayList<>();
+        String currentSection = getIntent().getStringExtra("Section");
+        for (int i = 0; i < HomePageFragment.levelArrayMap.size(); i++) {
+            if (HomePageFragment.levelArrayMap.get(i).keySet().iterator().hasNext()){
+                if (HomePageFragment.levelArrayMap.get(i).keySet().iterator().next().equals(currentSection)){
+                    phrases=HomePageFragment.levelArrayMap.get(i).get(currentSection);
+                }
+            }
+        }
+
+        return new ArrayList<>();
     }
 
 }
