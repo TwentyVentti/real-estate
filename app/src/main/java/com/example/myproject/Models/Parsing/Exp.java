329e0c26297084ff6475f8fdd2bcdd52f816c467
Andrew Carse <andrew.carse25@gmail.com>
2020-10-22 17:37:46 +1100

    Docstringing Exp.java

--- app/src/main/java/com/example/myproject/Models/Parsing/Exp.java
+++ app/src/main/java/com/example/myproject/Models/Parsing/Exp.java
@@ -2,6 +2,12 @@ package com.example.myproject.Models.Parsing;
 
 import java.io.Serializable;
 
+/**
+ * Abstract class that is implemented in Parser.
+ * Allows for inheritance when parsing for optimal object oriented programming.
+ *
+ * @author Andrew Carse u6666440
+ */
 public abstract class Exp implements Serializable {
     public abstract String[] evaluate();
     public abstract String show();
