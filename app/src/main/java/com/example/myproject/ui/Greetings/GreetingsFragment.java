10e0673ea1b6f552b2b65a1bd6cb0a584ca5b9db
kanav <u6995005@anu.edu.au>
2020-10-23 22:43:56 +1100

    greeting fragment update

--- app/src/main/java/com/example/myproject/ui/Greetings/GreetingsFragment.java
+++ app/src/main/java/com/example/myproject/ui/Greetings/GreetingsFragment.java
@@ -16,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import com.example.myproject.Models.JsonParsermap;
+import com.example.myproject.Models.JsonParsermap;
 import com.example.myproject.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -45,13 +46,6 @@ import java.util.Objects;
 
 /**
  * @author Kanav Thareja  - u6995005
- * The activity first first asks the user for location permsisions to load the app.It then goes on
- * to create the map from the google API. It firstly retrieves the user's current location coordinate
- * and displays it as a "Blue" marker with the text "I am here right now". An adapter dropdown stores
- * all the possible options for the user to search   On the the basis of users selection in the
- * dropdown it searches for the relevant places (subject to constraints) on buton click which with
- * the help of the google api. It displays the relevant markers for the searched place and clears
- * the previous markers everytime a new search is executed
  */
 
 public class GreetingsFragment extends Fragment {
@@ -64,6 +58,12 @@ public class GreetingsFragment extends Fragment {
     Marker mCurrLocationMarker;
     FusedLocationProviderClient fusedLocationProviderClient;
     double currentLat = 0, currentLong = 0;
+    private final int ZOOMVALUE=15;
+    private final int RADIUSVALUE=5000;
+    private final int BEARINGVALUE=90;
+    private final int TILTVALUE=40;
+    private final int REQUESTCODEVALUE=44;
+
 
 
     @Override
@@ -79,16 +79,28 @@ public class GreetingsFragment extends Fragment {
                 .findFragmentById(R.id.google_map);
 
 
+
+        ;
+
+
         final String[] placeTypeList = {"restaurant", "bar", "hotel","atm"};
         //search parameters
 
         String[] placeNameList = {"Looking for a place to eat?", "Looking for a place to drink?", "Looking for a place to stay?","Looking for a place to withdraw money?"};
         //Search options for user
 
+
+        /**An adapter dropdown stores all the possible options for the user to search as a dropdown**/
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, placeNameList);
         spType.setAdapter(adapter); //for dropdown of options
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity());
+
+        /**
+         * The activity first first asks the user for location permsissions to load the app.It then goes on
+         * to create the map from the google API.
+         **/
 
         if (ActivityCompat.checkSelfPermission(getActivity(),  // checking requesting permission
                 Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
@@ -98,6 +110,10 @@ public class GreetingsFragment extends Fragment {
 
         }
 
+        /**
+         *On the the basis of users selection in the dropdown it searches for the relevant places
+         * (subject to constraints) on button click which with the help of the google api
+         */
         btfind.setOnClickListener(new View.OnClickListener() { // on clicking "Find"
             @Override
 
@@ -105,7 +121,7 @@ public class GreetingsFragment extends Fragment {
                 int i = spType.getSelectedItemPosition();
                 String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" +  // string to search places
                         "?location=" + currentLat + "," + currentLong + //current location
-                        "&radius=5000" +    //Max area to search
+                        "&radius=RADIUSVALUE" +    //Max area to search
                         "&types=" + placeTypeList[i] +  //attribute to search
                         "&sensor=true" +  //Indicates whether or not the geocoding request comes from a device with a location sensor
                         "&key=" + getResources().getString(R.string.google_map_key);  // Google key for maps api
@@ -118,18 +134,32 @@ public class GreetingsFragment extends Fragment {
         return v; // return view
     }
 
+
+
     private void getCurrentLocation() {
+
+
         @SuppressLint("MissingPermission")
         Task<Location> task = fusedLocationProviderClient.getLastLocation(); //to retrieve the device's last known location
 
         task.addOnSuccessListener(new OnSuccessListener<Location>() { //called when location found
             @Override
             public void onSuccess(Location location) {
+
+
+
                 if (location!=null)
                 {
                     currentLat=location.getLatitude();
 
                     currentLong=location.getLongitude();
+
+
+
+                    /**It  retrieves the user's current location coordinate
+                     * and displays it as a "Blue" marker with the text "I am here right now".
+                     **/
+
                     supportMapFragment.getMapAsync(new OnMapReadyCallback() {  //initializing the maps system
                         @Override
                         public void onMapReady(GoogleMap googleMap) {
@@ -147,9 +177,9 @@ public class GreetingsFragment extends Fragment {
 
                             CameraPosition cameraPosition = new CameraPosition.Builder()
                                     .target(latLng)      // Sets the center of the map to location user
-                                    .zoom(15)            // zoom in on location
-                                    .bearing(90)                // Sets the orientation of the camera to east
-                                    .tilt(40)                   // Sets the tilt of the camera
+                                    .zoom(ZOOMVALUE)            // zoom in on location
+                                    .bearing(BEARINGVALUE)                // Sets the orientation of the camera to east
+                                    .tilt(TILTVALUE)                   // Sets the tilt of the camera
                                     .build();                   // Creates a CameraPosition from the builder
                             map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
@@ -159,6 +189,11 @@ public class GreetingsFragment extends Fragment {
                             uiSettings.setMapToolbarEnabled(true);
                             uiSettings.setZoomControlsEnabled(true);
                             uiSettings.setCompassEnabled(true);
+
+
+
+
+
                         }
 
 
@@ -169,10 +204,14 @@ public class GreetingsFragment extends Fragment {
             }
         });
     }
+
+
+
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         //permission check , If request is cancelled, the result is empty.
-        if (requestCode == 44){
+        if (requestCode == REQUESTCODEVALUE){
             if(grantResults.length > 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
                 getCurrentLocation();
         }
@@ -196,6 +235,10 @@ public class GreetingsFragment extends Fragment {
             new ParserTask().execute(s);
         }
     }
+
+    /**
+     * It downloads the url in the background.
+     */
 
     private String downloadUrl(String string) throws IOException { //downloading the url for places
 
@@ -238,10 +281,16 @@ public class GreetingsFragment extends Fragment {
             return mapList;
         }
 
+        /**
+         *after background task is executed,it clears all previous markers and displays new ones
+         * based on the search
+         */
+
+        @SuppressLint("MissingPermission")
         @Override
         protected void onPostExecute(List<HashMap<String, String>> hashMaps) { //after background task is executed
 
-                map.clear(); // clear all previous markers
+            map.clear(); // clear all previous markers
 
             for (int i=0; i<hashMaps.size(); i++)
             {
