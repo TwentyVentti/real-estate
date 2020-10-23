339851e754aa26997ef174b87a7ce4f443f95584
Andrew Carse <andrew.carse25@gmail.com>
2020-10-23 22:46:07 +1100

    Renaming gallary to Maps as it is more intuitive

--- app/src/main/java/com/example/myproject/Models/SearchDetails.java
+++ app/src/main/java/com/example/myproject/Models/SearchDetails.java
@@ -1,5 +1,9 @@
 package com.example.myproject.Models;
 
+/**
+ * This class provides us with efficient access to keeping track of the users
+ * search details while they navigate our other features of the app.
+ */
 public class SearchDetails {
     public String city;
     public String country;
